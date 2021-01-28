549f30d5fb8da4e85ca57e0dff034efd7e6fa870
Laurent <laurent.corriveau@gmail.com>
2021-01-28 16:10:25 -0500

    Création Super Feature

---
+++ HeadSpace_Unity/Assets/LC_Feature.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LC_Feature : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Ne vous laissez pas bernez par le vide. Ce feature est INCROYABLE!!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
