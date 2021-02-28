c5e5b6a8dfd575b4c47cc95e39ff0e8629b18465
Francis Rufiange <francis.rufiange@live.ca>
2021-02-28 18:09:40 -0500

    Corrections markers et curseur, rework de la scene Managers

--- HeadSpace_Unity/Assets/Scripts/ObjectsSystem/MovableMarker.cs
+++ HeadSpace_Unity/Assets/Scripts/ObjectsSystem/MovableMarker.cs
@@ -4,12 +4,6 @@ using UnityEngine;
 
 public class MovableMarker : MovableObject
 {
-    // Start is called before the first frame update
-    private void Start()
-    {
-
-    }
-
     protected override void Update()
     {
         base.Update();
@@ -18,7 +12,7 @@ public class MovableMarker : MovableObject
     public override void Select()
     {
         base.Select();
-        _mouseOffset = new Vector2(0.25f, 0);
+        _mouseOffset = new Vector2(0.04f, 0f);
         
         
     }
