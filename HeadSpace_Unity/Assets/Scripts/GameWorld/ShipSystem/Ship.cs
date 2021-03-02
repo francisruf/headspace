8c74175821fac4394af6ce2dd77b0eaeaf769f63
Laurent <laurent.corriveau@gmail.com>
2021-03-02 16:57:13 -0500

    ships added + stats

--- HeadSpace_Unity/Assets/Scripts/GameWorld/ShipSystem/Ship.cs
+++ HeadSpace_Unity/Assets/Scripts/GameWorld/ShipSystem/Ship.cs
@@ -10,13 +10,52 @@ using UnityEngine;
 
 public class Ship : MonoBehaviour
 {
+    public string shipName;
+    [Header("Stats")]
+
+    [Range(0,100)]
+    public int healthPoints;
+
+    [Range(0, 100)]
+    public int cargoCapacity;
+
+    [Range(0, 10)]
+    public float movementSpeed;
+
+    [Range(0, 10)]
+    //How many Souls per second can the Ship transfer from the Planet to Cargo
+    public float pickupSpeed;
+
+    [Range(0, 1)]
+    public float detectionRadius;
+
+    public CircleCollider2D detectionZone;
+
+    public Vector2 testGridCoords;
+
     void Start()
     {
-        
+
     }
 
     void Update()
     {
-        
+        //if (Input.GetKeyDown(KeyCode.M)) {
+        //    Move(testGridCoords);
+        //}
     }
+
+    //public void Move(Vector2 gridCoords) {
+
+    //}
+
+    //STEP 1: MOVE THAT SHIP
+    //Créer une fonction MOVE qui prend un Vecto2 à l'entrée (GridCoords)
+    //Coder le mouvement pour que ça marche independemment ici
+    //Créer un champs public Vector2 qui prend des GridCoords
+    //Convertir les GridCoords en WorldCoords (le faire DANS l'input)
+    //Quand j'INPUT qqc, vaisseau bouge mouvement speed vers ces coordonnées
+
+    //STEP 2: 
+    //
 }
---
+++ HeadSpace_Unity/Assets/Scripts/GameWorld/ShipSystem/Ship_Detection_Radius.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ship_Detection_Radius : MonoBehaviour
+{
+    CircleCollider2D detectionCollider;
+    public float detectionRadius;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        detectionCollider = GetComponent<CircleCollider2D>();
+        detectionCollider.radius = detectionRadius;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (Input.GetKeyDown(KeyCode.P)) {
+        //    detectionRadius.radius += 1f;
+        //}
+        detectionCollider.radius = detectionRadius;
+    }
+}
