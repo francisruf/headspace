87846755e7710f3065f2386bd4698154118cf900
Laurent <laurent.corriveau@gmail.com>
2021-03-12 10:01:55 -0500

    Started Receptor

---
+++ HeadSpace_Unity/Assets/Scripts/DeskObjects/ObjectsSystem/MovableMessage.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovableMessage : MovableObject
+{
+    public override void Select() {
+        base.Select();
+        //Send action to receptor that he can print the next message
+    }
+}
---
+++ HeadSpace_Unity/Assets/Scripts/DeskObjects/SlidableTools/Receptor.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Receptor : MonoBehaviour
+{
+
+    public GameObject messagePrefab;
+    public Transform printPoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P)) {
+            PrintMessage();
+        }
+    }
+
+    //Si le recepteur est sorti: Print message (Print next message in Queue plus tard)
+
+    private void PrintMessage() {
+        GameObject message = Instantiate(messagePrefab, printPoint.position, printPoint.rotation, printPoint);
+    }
+}
