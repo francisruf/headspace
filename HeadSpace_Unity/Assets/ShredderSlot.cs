947f5bba24130fda0081f26b6ce0e3a5c38e61d0
Ben <benlem@globetrotter.net>
2021-03-12 11:24:17 -0500

    Shredder

---
+++ HeadSpace_Unity/Assets/ShredderSlot.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ShredderSlot : MonoBehaviour
+{   
+    public Transform EndPosition;
+    public SpriteRenderer GreenButton;
+    public SpriteRenderer RedButton;
+    private SlidableShredder parent = null;
+    private bool shredding = false;
+
+    private void Start()
+    {
+        GreenButton.enabled = false;
+        RedButton.enabled = true;
+
+        if (parent == null)
+        {
+            parent = transform.parent.GetComponent<SlidableShredder>();
+        }
+    }
+    private void Update()
+    {
+        if (!CanShred() || shredding)
+        {
+            GreenButton.enabled = false;
+            RedButton.enabled = true;
+        }
+        else
+        {
+            GreenButton.enabled = true;
+            RedButton.enabled = false;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == 14 && collision.gameObject.tag == "Destruc")
+
+        {
+            if(CanShred() && !shredding){ 
+                collision.GetComponent<MovableObject>().Deselect();
+                collision.transform.position = new Vector3(collision.transform.position.x, transform.position.y, collision.transform.position.z);
+                GreenButton.enabled = false;
+                RedButton.enabled = true;
+                StartCoroutine(Shred(collision.gameObject));
+                FindObjectOfType<AudioManager>().PlaySound("Shredder");
+            }
+        }
+
+        if (collision.gameObject.layer == 14 && collision.gameObject.tag == "Indestruc")
+        {
+            GreenButton.enabled = false;
+            RedButton.enabled = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == 14 && collision.gameObject.tag == "Destruc")
+        {
+            GreenButton.enabled = true;
+            RedButton.enabled = false;
+        }
+        if (collision.gameObject.layer == 14 && collision.gameObject.tag == "Indestruc")
+        {
+            GreenButton.enabled = true;
+            RedButton.enabled = false;
+        }
+    }
+
+    private IEnumerator Shred(GameObject doc)
+    {
+        shredding = true;
+        yield return StartCoroutine(LerpToPosition(doc));
+        shredding = false;
+        doc.GetComponent<MovableObject>().DisableObject();
+    }
+
+    private IEnumerator LerpToPosition(GameObject doc)
+    {
+        Vector3 startingPosition = doc.transform.position;
+        Vector3 endingPosition = EndPosition.position; 
+        float timeElaps = 0f;
+        float duration = 3f;
+        while(timeElaps < duration)
+        {
+            doc.transform.position = Vector3.Lerp(startingPosition, endingPosition, timeElaps / duration);
+            timeElaps += Time.deltaTime;
+            yield return null;
+        }
+    }
+    private bool CanShred()
+    {   //Determine if we can shred based on the position of the shredder compared to the total width of the camera
+        float neededRatioValue = 0.15f;
+        float ratio = 1 - (Mathf.Abs(parent.transform.position.x) / (Camera.main.orthographicSize * 2));
+        return ratio > neededRatioValue;
+    }
+}
---
+++ HeadSpace_Unity/Assets/SlidableShredder.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlidableShredder : SlidableTool
+{
+    private Vector3 mouseOffset;
+    private float boxHalfSize;
+    private ShredderSlot child = null;
+    
+    // Start is called before the first frame update
+    protected override void Start()
+    {
+        base.Start();
+        child = transform.GetComponentInChildren<ShredderSlot>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_isSelected)
+        {
+            if (Input.GetMouseButton(0))
+            {
+                Vector3 mousePos;
+
+                mousePos = Input.mousePosition;
+                mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+
+                float vertExtent = Camera.main.orthographicSize;
+                float horExtent = vertExtent * Screen.width / Screen.height;
+
+                switch (slidingDirection)
+                {
+                    case SlidingDirection.HorizontalLeft:
+                        float clampedXPosLeft = Mathf.Clamp(mousePos.x + mouseOffset.x, horExtent - (slidingAmount / 2f), horExtent + (slidingAmount / 2f) - drawerHandleSize);
+                        gameObject.transform.position = new Vector3(clampedXPosLeft, transform.position.y, 0);
+                        break;
+
+                    case SlidingDirection.HorizontalRight:
+                        float clampedXPosRight = Mathf.Clamp(mousePos.x + mouseOffset.x, -horExtent - (slidingAmount / 2f) + drawerHandleSize, -horExtent + (slidingAmount / 2f));
+                        gameObject.transform.position = new Vector3(clampedXPosRight, transform.position.y, 0);
+                        break;
+
+                    case SlidingDirection.VerticalDown:
+                        float clampedYPosDown = Mathf.Clamp(mousePos.y + mouseOffset.y, vertExtent - (slidingAmount / 2f), vertExtent + boxHalfSize - drawerHandleSize);
+                        gameObject.transform.position = new Vector3(transform.position.x, clampedYPosDown, 0);
+                        break;
+
+                    case SlidingDirection.VerticalUp:
+                        float clampedYPosUp = Mathf.Clamp(mousePos.y + mouseOffset.y, -vertExtent - (slidingAmount / 2f) + drawerHandleSize, -vertExtent + boxHalfSize);
+                        gameObject.transform.position = new Vector3(transform.position.x, clampedYPosUp, 0);
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+            else
+            {
+                Deselect();
+            }
+        }
+    }
+    public override void Select()
+    {
+        base.Select();
+        if(child != null)
+        {
+            //Correct the sorting order in layer to always show the shredder slot
+            child.GetComponent<SpriteRenderer>().sortingOrder = GetComponent<SpriteRenderer>().sortingOrder + 1;
+        }
+    }
+}
