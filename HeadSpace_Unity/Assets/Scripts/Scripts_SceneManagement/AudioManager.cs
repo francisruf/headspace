45a324ad4f0e5539265c9d1d6dc3170a16066754
Francis Rufiange <francis.rufiange@live.ca>
2021-04-02 16:49:26 -0400

    Added fadeIn-Out to AudioManager

--- HeadSpace_Unity/Assets/Scripts/Scripts_SceneManagement/AudioManager.cs
+++ HeadSpace_Unity/Assets/Scripts/Scripts_SceneManagement/AudioManager.cs
@@ -1,28 +1,39 @@
 ﻿using UnityEngine.Audio;
 using UnityEngine;
 using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 
-public class AudioManager : MonoBehaviour {
-    public Sound[] sounds;
+public class AudioManager : MonoBehaviour
+{
     public static AudioManager instance;
-    [HideInInspector]
-    public Sound currentMusic;
+
+    [Header("Settings")]
+    public float sfxFadeSpeed;
+    public float musicFadeSpeed;
+
+    [Header("Sound DB")]
+    public Sound[] sounds;
+    [HideInInspector] public Sound currentMusic;
 
     // Start is called before the first frame update
-    void Awake() {
-        if (instance == null) {
+    void Awake()
+    {
+        if (instance == null)
+        {
             instance = this;
         }
-
-        else {
+        else
+        {
             Destroy(gameObject);
             return;
         }
 
         DontDestroyOnLoad(gameObject);
 
-        foreach (Sound s in sounds) {
+        foreach (Sound s in sounds)
+        {
             s.source = gameObject.AddComponent<AudioSource>();
             s.source.clip = s.clip;
 
@@ -75,7 +86,8 @@ public class AudioManager : MonoBehaviour {
       //  }
     }
      
-    private void AssignMusicOnScene(Scene scene1, Scene scene2) {
+    private void AssignMusicOnScene(Scene scene1, Scene scene2)
+    {
 
         //if (SceneManager.GetActiveScene().name == "MainMenu") {
         //    PlayTheme("MainMenu_Loop");
@@ -88,26 +100,33 @@ public class AudioManager : MonoBehaviour {
         //}
     }
 
-    public void PlaySound(string name) {
+    public void PlaySound(string name, bool fadeIn = false)
+    {
         Sound s = Array.Find(sounds, sound => sound.name == name);
-        if (s == null) {
+        if (s == null)
+        {
             Debug.LogWarning("Sound: " + name + " not found!");
             return;
         }
 
-        s.source.Play();
+        if (fadeIn)
+            StartCoroutine(FadeIn(s));
+
+        else
+            s.source.Play();
     }
 
-    public void PlayTheme(string name) {
+    public void PlayTheme(string name)
+    {
         Sound s = Array.Find(sounds, sound => sound.name == name);
-        if (s == null) {
+        if (s == null)
+        {
             Debug.LogWarning("Sound: " + name + " not found!");
             return;
         }
 
-        if (currentMusic.source != null) {
+        if (currentMusic.source != null)
             currentMusic.source.Stop();
-        }
 
         currentMusic = s;
         currentMusic.source.Play();
@@ -118,6 +137,47 @@ public class AudioManager : MonoBehaviour {
         return Array.Find(sounds, sound => sound.name == name);
     }
 
+    private IEnumerator FadeIn(Sound sound)
+    {
+        float targetVolume = sound.volume;
+        sound.source.volume = 0f;
+        float time = 0f;
+
+        sound.source.Play();
+
+        while (time < sfxFadeSpeed)
+        {
+            if (Mathf.Approximately(targetVolume, sound.source.volume))
+            {
+                break;
+            }
+
+            sound.source.volume = Mathf.Clamp(sound.source.volume + (Time.deltaTime / sfxFadeSpeed * targetVolume), 0f, targetVolume);
+            time += Time.deltaTime;
+            yield return new WaitForEndOfFrame();
+        }
+    }
+
+    private IEnumerator FadeOut(Sound sound)
+    {
+        float startVolume = sound.source.volume;
+        float time = 0f;
+
+        while (time < sfxFadeSpeed)
+        {
+            if (Mathf.Approximately(0f, sound.source.volume))
+            {
+                break;
+            }
+
+            sound.source.volume = Mathf.Clamp(sound.source.volume - (Time.deltaTime / sfxFadeSpeed * startVolume), 0f, startVolume);
+            time += Time.deltaTime;
+            yield return new WaitForEndOfFrame();
+        }
+        Debug.Log("FADE OUT END.");
+        Debug.Log("TIME VALUE : " + time);
+        Debug.Log("VOL. VALUE : " + sound.source.volume);
+    }
 
     private void ShredderStarted()
     {
@@ -126,7 +186,7 @@ public class AudioManager : MonoBehaviour {
             if (shredSound.source.isPlaying)
                 return;
 
-        PlaySound("Shredder");
+        PlaySound("Shredder", true);
     }
 
     private void ButtonPress()
